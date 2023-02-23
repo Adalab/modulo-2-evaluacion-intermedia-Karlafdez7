38c233683a96629ab230f0c9ad3a77cf4df79e32
Karla <karlafernandezchirinos@gmail.com>
2023-02-23 10:05:14 +0100

    add correction_issues_javascript-basic/advanced/good-practices_Html_Css

--- js/main.js
+++ js/main.js
@@ -4,33 +4,41 @@ const numberSelection = document.querySelector(".js-number");
 const buttonSelection = document.querySelector(".js-btn");
 const messageAdvice = document.querySelector(".js-message");
 const tryUser = document.querySelector(".js-try");
+const number = getRandomNumber(100);
+let attempts = 1;
 
 function getRandomNumber(max) {
     const numberRandom = Math.ceil(Math.random() * max);
     return numberRandom; 
 }  
 
+function userMessage (message) {
+    messageAdvice.innerHTML = message;
+}
+
 function useRandomNumber() {
-    const userNumber = numberSelection.value; 
-    const number = getRandomNumber(100);
-    if (userNumber > number) {
-        messageAdvice.innerHTML = "Demasiado alto.";
+    const userNumber = parseInt(numberSelection.value); 
+
+    if (isNaN(userNumber)){
+         userMessage("Introduzta un valor numérico.");
+    }
+    else if (userNumber > number) {
+        userMessage("Demasiado alto.");
         if (userNumber > 100) {
-            messageAdvice.innerHTML = "El número debe esta entre 1 y 100.";
+            userMessage("El número debe esta entre 1 y 100.");
         }
     } else if (userNumber < number) {
-        messageAdvice.innerHTML = "Demasiado bajo.";
+       userMessage("Demasiado bajo.");
         if (userNumber < 1) {
-            messageAdvice.innerHTML = "El número debe esta entre 1 y 100.";
+            userMessage ("El número debe esta entre 1 y 100.");
         }
     } else if (userNumber === number) {
-        messageAdvice.innerHTML = "Has ganado campeona!!!";
+        userMessage("Has ganado campeona!!!");
     } 
      console.log(number);
 }
 
-const countAttemps = getRandomNumber(100);
-let attempts = 1;
+
 
 function tryAgain() {
     const totalTries = attempts++;
